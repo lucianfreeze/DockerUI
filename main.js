c9650e427a03c3cdcaf50666d6b19e8338b9e960
Garrett Hay <36011099+Garrett-H@users.noreply.github.com>
2019-09-29 04:51:54 -0400

    added documentation to electron main.js

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
-const {
-  app,
-  BrowserWindow
-} = require('electron')
+/**
+ * @fileoverview The main.js file to house and
+ * run electron and application from.
+ * @author Lucian Freeze
+ * @author Garrett Hay
+ * @author Brett Whitson
+ * 
+ * @requires NPM:node_modules
+ * 
+ */
 
-var Docker = require('dockerode');
-
-var docker = new Docker({
-  host: '127.0.0.1',
-  port: 2375
-});
+ /**
+ * main importation of Electron
+ * @constant
+ * 
+ * @type {Electron}
+ */
+const {app, BrowserWindow} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
 
+/**
+ * global reference of window object
+ * @type {Electron.BrowserWindow}
+ */
+let win;
+
+/**
+ * Creates the specification of a browser window,
+ * allows node integration into the window,
+ * loads the html into the electron app,
+ * opens Developer's tools,
+ * and what to do when window is closed.
+ */
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
@@ -22,13 +41,13 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true
     }
-  })
+  });
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile('index.html');
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  win.webContents.openDevTools();
 
 
   // Emitted when the window is closed.
@@ -36,14 +55,21 @@ function createWindow() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
-    app.quit()
-  })
+    win = null;
+    app.quit();
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
+
+/**
+ * if statement for starting application.
+ * as long as app exist it starts with createWindow method.
+ * When all windows are closed the application quits.
+ * when application activates run createWindow method.
+ */
 if (app != null) {
   app.on('ready', createWindow);
 
@@ -52,7 +78,7 @@ if (app != null) {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-      app.quit()
+      app.quit();
     }
   });
 
@@ -60,7 +86,7 @@ if (app != null) {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (win === null) {
-      createWindow()
+      createWindow();
     }
   });
 }
